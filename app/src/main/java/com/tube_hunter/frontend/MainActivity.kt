d7bc6f91befddf5c77ed1402970837daf8fc16ee
EmilieVS <emivsy@gmail.com>
2025-08-19 15:49:06 +0200

    add:spotlist background and cards

--- app/src/main/java/com/tube_hunter/frontend/MainActivity.kt
+++ app/src/main/java/com/tube_hunter/frontend/MainActivity.kt
@@ -2,12 +2,10 @@ package com.tube_hunter.frontend
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,23 +13,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.os.postDelayed
 import androidx.lifecycle.lifecycleScope
-import com.tube_hunter.frontend.ui.theme.FrontendTheme
 import com.tube_hunter.frontend.ui.theme.WhiteFoam
 import com.tube_hunter.frontend.ui.theme.chewy
 import kotlinx.coroutines.delay
--- app/src/main/java/com/tube_hunter/frontend/SpotsListActivity.kt
+++ app/src/main/java/com/tube_hunter/frontend/SpotsListActivity.kt
@@ -1,7 +1,99 @@
 package com.tube_hunter.frontend
 
-import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tube_hunter.frontend.ui.theme.WhiteFoam
+import com.tube_hunter.frontend.ui.theme.chewy
 
-class SpotsListActivity : Activity() {
+class SpotsListActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BackgroundLoading()
 
+        }
+    }
+}
+@Preview
+@Composable
+fun BackgroundLoading() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.background_tube_hunter),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ){
+
+        Text(
+            text = "TUBE HUNTER",
+            textAlign = TextAlign.Center,
+            fontSize = 48.sp,
+            fontFamily = chewy,
+            color = WhiteFoam,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+            SpotCard()
+    }
+}
+    }
+@Preview
+@Composable
+fun SpotCard() {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 300.dp),
+
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.background_tube_hunter),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(width = 240.dp, height = 140.dp)
+                .padding(24.dp),
+        )
+        Text(
+            text = "Filled",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+
+    }
 }
