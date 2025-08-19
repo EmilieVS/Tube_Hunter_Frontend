3f160fbc1fe204542c71d8695aae60628fde1fd7
Loic Desbordes <loic.du.69@hotmail.fr>
2025-08-19 11:51:19 +0200

    add: style

--- app/src/main/java/com/tube_hunter/frontend/MainActivity.kt
+++ app/src/main/java/com/tube_hunter/frontend/MainActivity.kt
@@ -4,44 +4,79 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tube_hunter.frontend.ui.theme.FrontendTheme
+import com.tube_hunter.frontend.ui.theme.WhiteFoam
+import com.tube_hunter.frontend.ui.theme.chewy
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FrontendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HomeScreen()
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // ✅ Image de fond
+        Image(
+            painter = painterResource(id = R.drawable.background_tube_hunter),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FrontendTheme {
-        Greeting("Android")
+        // ✅ Contenu par-dessus
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_light_tube_hunter),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+            )
+            Text(
+                text = "TUBE HUNTER",
+                textAlign = TextAlign.Center,
+                fontSize = 104.sp,
+                fontFamily = chewy,
+                color = WhiteFoam,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
     }
 }
--- app/src/main/java/com/tube_hunter/frontend/ui/theme/Color.kt
+++ app/src/main/java/com/tube_hunter/frontend/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val WhiteFoam = Color(0xFFF3F3EA)
+
+val LagoonBlue = Color(0xFF3BA9A4)
+
+val DrySand = Color(0xFFE5D3B3)
