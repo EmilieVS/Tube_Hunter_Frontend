702a3b7393159d2716a01e66bc005b13d87e6b58
AxelleBdd <axbaudinaud@gmail.com>
2025-09-05 15:46:05 +0200

    wip: start of the searchbar filter

--- app/src/main/java/com/tube_hunter/frontend/ui/screen/spotlist/SpotListScreen.kt
+++ app/src/main/java/com/tube_hunter/frontend/ui/screen/spotlist/SpotListScreen.kt
@@ -25,9 +25,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DockedSearchBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -324,6 +327,35 @@ fun FilterDialog(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(){
+    DockedSearchBar(
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = query,
+                onQueryChange = onQueryChange,
+                onSearch = onSearch,
+                expanded = active,
+                onExpandedChange = onActiveChange,
+                enabled = enabled,
+                placeholder = placeholder,
+                leadingIcon = leadingIcon,
+                trailingIcon = trailingIcon,
+                colors = colors.inputFieldColors,
+                interactionSource = interactionSource,
+            )
+        },
+        expanded = active,
+        onExpandedChange = onActiveChange,
+        modifier = modifier,
+        shape = shape,
+        colors = colors,
+        tonalElevation = tonalElevation,
+        shadowElevation = shadowElevation,
+        content = content,
+    )
+}
 
 @Composable
 fun IconDifficulty(rating: Int) {
